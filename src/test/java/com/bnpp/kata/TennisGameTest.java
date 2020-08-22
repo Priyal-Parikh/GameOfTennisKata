77eec7723e68aba0844519fb628e0349756d5266
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-22 21:15:38 +0530

    Add: Initialize Tennis Game

---
+++ src/test/java/com/bnpp/kata/TennisGameTest.java
@@ -0,0 +1,14 @@
+package com.bnpp.kata;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TennisGameTest
+{
+    @Test
+    public void initializeNewTennisGame() {
+        TennisGame tennisGame = new TennisGame();
+
+        Assert.assertNotNull(tennisGame);
+    }
+}
